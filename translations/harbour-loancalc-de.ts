323aa7afdaca80f1e13268f63c4b88480f5870ad
Alexey Svistunov <svalx@svalx.net>
2018-12-09 22:25:44 +0400

    Название изменил на обложке

--- translations/harbour-loancalc-de.ts
+++ translations/harbour-loancalc-de.ts
@@ -1,11 +1,11 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.1">
 <context>
     <name>CoverPage</name>
     <message>
-        <source>My Cover</source>
-        <translation>Mein Cover</translation>
+        <source>Loan Calculator</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
